084b40dba9f84fe34542bcfb75517739c68afaa9
Adrienne F <adrienne_frnndz@yahoo.com>
2020-03-29 14:48:41 -0700

    working wireframe

---
+++ routes/apiRoutes.js
@@ -0,0 +1,15 @@
+//const db = require('../models')
+const express = require('express');
+const router = express.Router()
+
+    router.get('/api/homepage', (req,res)=>{
+
+    })
+
+    router.post('/challenge', (req,res)=>{
+        
+    })
+
+
+
+module.exports = router
