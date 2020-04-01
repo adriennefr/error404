a20d067736a642ae8c70326dffb6d8b31236afdf
Austin Rodrigues <austin.da.rodrigues@gmail.com>
2020-04-01 10:55:05 -0700

    created homepage buttons and app bar

--- client/src/components/Fitmoodji/Fitmoodji.js
+++ client/src/components/Fitmoodji/Fitmoodji.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Avatar from '@material-ui/core/Avatar';
-import './style.css'
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    '& > *': {
-      margin: theme.spacing(1),
-    },
+    flexGrow: 1,
+  },
+  grid: {
+    position: 'absolute', 
+    left: '50%', 
+    top: '30%',
+    transform: 'translate(-50%, -50%)'
+  },
+  
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    
   },
 }));
 
-export default function ImageAvatars() {
+export default function FullWidthGrid() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" className="fitmoodji" />
+      <Grid container spacing={4} >
+        <Grid item xs={4} alignContent="flex-start" direction="row" justify="flex-start" alignItems="center">
+          <Paper className={classes.paper} >Logo</Paper>
+        </Grid>
+        <Grid item xs={12} className={classes.grid} direction="row" justify="center" alignItems="center" >
+          <Paper className={classes.paper}>Avatar Goes Here</Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import HomeIcon from '@material-ui/icons/Home';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import FitnessCenterIcon from '@material-ui/icons/FitnessCenter';
+import GroupIcon from '@material-ui/icons/Group';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+        top: 'auto',
+        bottom: 0,
+    },
+
+}));
+
+export default function BottomAppBar() {
+    const classes = useStyles();
+
+    return (
+        <React.Fragment>
+            <AppBar position="fixed" color="primary" className={classes.appBar}>
+                <Toolbar>
+                    <Box mx="auto">
+                        <IconButton size='medium' color="inherit">
+                            <HomeIcon />
+                        </IconButton>
+                        <IconButton size="medium" color="inherit">
+                            <BarChartIcon />
+                        </IconButton>
+                        <IconButton size="medium" color="inherit">
+                            <FitnessCenterIcon />
+                        </IconButton>
+                        <IconButton size="medium" color="inherit">
+                            <GroupIcon />
+                        </IconButton>
+                        <IconButton size="medium" color="inherit">
+                            <AccountBoxIcon />
+                        </IconButton>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </React.Fragment>
+    );
+}
