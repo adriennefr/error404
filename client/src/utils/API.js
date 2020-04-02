fb129d8cd596f6ba89dc44d7ffde900e472afb6f
Austin Rodrigues <austin.da.rodrigues@gmail.com>
2020-04-02 15:14:35 -0700

    craeted logo component

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,7 @@ import axios from 'axios';
 export default {
     signUp: (obj)=> axios.post('/api/signup', obj),
     setGoals: (arr)=> axios.post('/api/goals', arr),
-    getGoals: ()=> axios.get('/api/goals')
+    getGoals: ()=> axios.get('/api/goals'),
+    renderAvatar: ()=> axios.get('/api/users')
 }
+
