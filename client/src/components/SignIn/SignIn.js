de7b9d4afc8344f1d34c5ab30d6f7c13dceefe01
Suzy <suzylee824@gmail.com>
2020-04-02 20:43:52 -0700

    styled login and signup pages

--- client/src/components/SignIn/SignIn.js
+++ client/src/components/SignIn/SignIn.js
@@ -43,6 +43,8 @@ const useStyles = makeStyles((theme) => ({
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
+    backgroundColor: "#85FFCE",
+    fontSize: 15,
   },
 }));
 
--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import SignUpCard from '../components/SignUpCard/SignUpCard';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+    background: {
+        backgroundColor: "#B8C0DB",
+        height: "130vh"
+    },
+}));
 
 function SignUpPage() {
+
+    const classes = useStyles();
     return(
-        <SignUpCard />
+        <Container className={classes.background}>
+            <SignUpCard />
+        </Container>
     )
 }
 
