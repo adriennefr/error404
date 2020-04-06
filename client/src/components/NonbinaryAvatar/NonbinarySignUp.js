4722367bc57dc4ecc05c5073feb6f6a9ab734a3e
Suzy <suzylee824@gmail.com>
2020-04-05 21:15:55 -0700

    changed the img src for sign up avatars

--- client/src/components/NonbinaryAvatar/NonbinarySignUp.js
+++ client/src/components/NonbinaryAvatar/NonbinarySignUp.js
@@ -4,8 +4,8 @@ import './style.css'
 
 function NonbinarySignUp({ selected, onClick }) {
     return (
-        selected ? <img alt="nonbinary avatar" className="nonbinarySignUp selected" src={'/images/non.jpg'} />
-            : <img onClick={() => onClick()} alt="nonbinary avatar" className="nonbinarySignUp" src={'/images/non.jpg'} />
+        selected ? <img alt="nonbinary avatar" className="nonbinarySignUp selected" src={'/images/clicked-nonbinary.png'} />
+            : <img onClick={() => onClick()} alt="nonbinary avatar" className="nonbinarySignUp" src={'/images/non.JPG'} />
     )
 }
 
